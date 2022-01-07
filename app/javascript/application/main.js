dd03e94978dde9863e0eb3c28841710b5862ae48
mthlucena <mthlucena@gmail.com>
2022-01-06 22:52:11 -0300

    Entrega 3: avaliacao criada, invite logic ready e ajustes finais

--- app/javascript/application/main.js
+++ app/javascript/application/main.js
@@ -1,6 +1,9 @@
-window.onload=function(){
-    if(document.getElementById("teamSelector")) {
+window.addEventListener("wheel", function(e) {
 
+    
+    // HOME DROPDOWN
+    if(document.getElementById("teamSelector")) {
+        
         teamSelector = document.getElementById("teamSelector");
         
         teamSelector.addEventListener('change', update)
@@ -8,9 +11,11 @@ window.onload=function(){
         function update(e) {
             team = document.getElementById("scrim_team");
             team.value = teamSelector.value;
+            console.log(team.value)
         }
     }
-
+    
+    // SEARCH BAR
     if(document.getElementById("teamInput")) {
         teamInput = document.getElementById("teamInput")
         
@@ -31,4 +36,33 @@ window.onload=function(){
             });
         }
     }
-}
+    
+    // SEARCH DROPDOWN ENEMY
+    if(document.getElementById("enemySelector")) {
+        
+        enemySelector = document.getElementById("enemySelector");
+        
+        enemySelector.addEventListener('change', update)
+        
+        function update(e) {
+            team1 = document.getElementById("invite_team1");
+            team1.value = enemySelector.value;
+            console.log(team1.value);
+        }
+    }
+    
+    //SEARCH DROPDOWN MY TEAM
+    if(document.getElementById("myTeamSelector")) {
+        
+        myTeamSelector = document.getElementById("myTeamSelector");
+        
+        myTeamSelector.addEventListener('change', update)
+        
+        function update(e) {
+            team2 = document.getElementById("invite_team2");
+            team2.value = myTeamSelector.value;
+            console.log(team2.value);
+        }
+    }
+    
+});
